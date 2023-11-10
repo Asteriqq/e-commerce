ae158e79b05df37632793f5bc800b054b465d9eb
Asteriqq <shayofunmifemi@gmail.com>
2023-11-10 14:23:18 +0100

    commit

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ if (close){
         nav.classList.remove('active')
     })
 }
+
+let MainImg = document.getElementById('MainImg')
+let SmallImg = document.getElementsByClassName('small-img')
+
+if (SmallImg[0]){
+    SmallImg[0].addEventListener('click', ()=>{
+        MainImg.src = SmallImg[0].src;
+    })
+}
+
+if (SmallImg[1]){
+    SmallImg[1].addEventListener('click', ()=>{
+        MainImg.src = SmallImg[1].src;
+    })
+}
+
+if (SmallImg[2]){
+    SmallImg[2].addEventListener('click', ()=>{
+        MainImg.src = SmallImg[2].src;
+    })
+}
+
+if (SmallImg[3]){
+    SmallImg[3].addEventListener('click', ()=>{
+        MainImg.src = SmallImg[3].src;
+    })
+}
